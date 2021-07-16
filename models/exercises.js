af3872b4f2c950b1c883ab14f3330082552be47f
Jessie Guadarrama <jesguadarrama98@gmail.com>
2021-07-15 21:03:04 -0700

    created day.js and index.js to set up models. set up sccording to seeds.js file

--- models/exercises.js
+++ models/exercises.js
@@ -2,7 +2,32 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
-const exerciseSchema = new Schema({});
+const exerciseSchema = new Schema({
+  type: {
+    type: String,
+  },
+  name: {
+    type: String,
+  },
+  duration: {
+    type: Number,
+  },
+  duration: {
+    type: Number,
+  },
+  weight: {
+    type: Number,
+  },
+  reps: {
+    type: Number,
+  },
+  sets: {
+    type: Number,
+  },
+  distance: {
+    type: Number,
+  },
+});
 
 const Exercises = mongoose.model('Exercises', exerciseSchema);
 
