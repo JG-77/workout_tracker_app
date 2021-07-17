46f377c9a9112fc41d17da7a7bee80833c20a0cc
Jessie Guadarrama <jesguadarrama98@gmail.com>
2021-07-17 10:03:52 -0700

    set up first api route'

--- routes/api.js
+++ routes/api.js
@@ -1,4 +1,15 @@
 const router = require('express').Router();
 const Workout = require('../models/workout');
 
+router.get('/api/workouts', (req, res) => {
+  Workout.find({})
+    .populate('exercises')
+    .then((dbWorkouts) => {
+      res.json(dbWorkouts);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
+
 module.exports = router;
