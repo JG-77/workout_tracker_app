97aa546d990d3d6e39532f223678621d117c68c0
Jessie Guadarrama <jesguadarrama98@gmail.com>
2021-07-16 20:27:51 -0700

    finally got /exercise html to load

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 var path = require('path');
 
 router.get('/exercise', (req, res) => {
-  res.sendFile(path.join(__dirname, '/exercise.html'));
+  res.sendFile(path.join(__dirname, '../public', 'exercise.html'));
 });
 
 module.exports = router;
